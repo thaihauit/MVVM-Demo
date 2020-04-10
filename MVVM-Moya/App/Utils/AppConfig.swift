e95e3a357b2c5af0cee3265cc3c190d5022878c1
D.Ace <thaihauit2771991@gmail.com>
2020-04-10 09:21:17 +0700

    remove file

--- MVVM-Moya/App/Utils/AppConfig.swift
+++ MVVM-Moya/App/Utils/AppConfig.swift
@@ -14,7 +14,7 @@ class AppConfig : NSObject {
 
 static let sharedInstance = AppConfig()
     
-    let baseURL = "http://ec2-3-112-198-223.ap-northeast-1.compute.amazonaws.com/bcom/api/"
+    let baseURL = ""
     static func getUuidDevice() -> String {
         return UIDevice.current.identifierForVendor?.uuidString ?? ""
     }
